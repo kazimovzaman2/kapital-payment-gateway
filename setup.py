ef47fe78c33921e2c76ce29d1584a9369e53f526
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-11-03 13:44:18 +0400

    Update readme file

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="kapital",
     version="0.1",
@@ -7,4 +10,6 @@ setup(
     install_requires=[
         "requests",
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
