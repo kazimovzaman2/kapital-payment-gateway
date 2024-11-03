c073b24892ba07798b99d3fda3c48512b92fa1aa
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-11-03 13:56:46 +0400

    Added author and maintainer, and also change version, also added description

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name="kapital",
-    version="0.2.0",
+    version="0.2.2",
+    description="A simple client for the Kapital payment gateway.",
     packages=find_packages(),
     install_requires=[
         "requests",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    author="Zaman Kazimov",
+    author_email="kazimovzaman2@gmail.com",
+    maintainer="Fuad Huseynov",
+    maintainer_email="fuadhuseynov@gmail.com",
 )
