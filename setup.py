61a988952a5a0d429ee93323b80b48c556c35bce
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-11-03 18:29:26 +0400

    Update version logic just get directly from github actions

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("VERSION", "r") as f:
+    version = f.read().strip()
+
 setup(
     name="kapital",
-    version="0.2.2",
+    version=version,
     description="A simple client for the Kapital payment gateway.",
-    packages=find_packages(),
-    install_requires=[
-        "requests",
-    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Zaman Kazimov",
     author_email="kazimovzaman2@gmail.com",
     maintainer="Fuad Huseynov",
     maintainer_email="fuadhuseynov@gmail.com",
+    url="https://github.com/kazimovzaman2/kapital-python",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
 )
